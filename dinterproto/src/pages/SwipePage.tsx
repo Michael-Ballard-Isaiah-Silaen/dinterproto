636f68c7a82c2ffcf9dc27be94830605253b8a38
Michael-Ballard-Isaiah-Silaen <silaenmichael@gmail.com>
2025-11-07 22:41:53 +0700

    Init 2

--- dinterproto/src/pages/SwipePage.tsx
+++ dinterproto/src/pages/SwipePage.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import Slider from "react-slick";
 import { FaSpotify, FaTiktok, FaHeart, FaTimes } from "react-icons/fa";
 import { MOCK_PROFILES } from "../data/mockData";
